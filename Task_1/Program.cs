10bf49a8a7f7016cf303b113ae6e980b45f3fab4
Alexandr Nesterenko <nesteronok@gmail.com>
2023-02-19 14:30:26 +0600

    Task 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Введите положительное пятизнаяное число");
+bool check = false;
+int number = 0;
+while (check == false)
+{
+    string text = Console.ReadLine();
+    if (int.TryParse(text, out number))
+    {
+        while (check == false)
+        {
+
+            if (number >= 10000 && number <= 99999)
+            {
+                check = true;
+
+            }
+            else
+            {
+                Console.WriteLine("Попробуйте ещё раз");
+                break;
+            }
+        }
+    }
+    else
+    {
+        Console.WriteLine("Не удалось распознать число, поробуйте ещё раз");
+    }
+}
+
+string numberString = Convert.ToString(number);
+
+if(numberString[0] == numberString[4] && numberString[1] == numberString[3])
+{
+    Console.WriteLine($"Число {number} - является палиндромом");
+} else
+{
+    Console.WriteLine($"Число {number} - НЕ является палиндромом");
+}
