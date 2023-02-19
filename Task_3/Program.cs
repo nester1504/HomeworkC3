d9fe836ff5c3093aeaa1ff05270f3722dbeb1829
Alexandr Nesterenko <nesteronok@gmail.com>
2023-02-19 14:56:25 +0600

    Task 3

---
+++ Task_3/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите число N: ");
+bool check = false;
+int number = 0;
+
+while (check == false)
+{
+    string text = Console.ReadLine();
+    if (int.TryParse(text, out number))
+    {
+        while (check == false)
+        {
+        
+            for (int i = 1; i <= number; i++)
+            {
+                Console.Write(Math.Pow(i, 3) + "\t");
+            }
+            check = true;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Не удалось распознать число, поробуйте ещё раз");
+    }
+}
