6b7ba70b57388b34b62a6757661cab527eacf5bf
Alexandr Nesterenko <nesteronok@gmail.com>
2023-02-20 08:58:48 +0600

    Task 6

---
+++ Task_6/Program.cs
@@ -0,0 +1,42 @@
+﻿int Interesting(int n)
+{
+    int mult = 1;
+    int sum = 0;
+    string numberSting;
+    int numberOne;
+    numberSting = Convert.ToString(n);
+    int size = Convert.ToInt32(numberSting.Length);
+    for (int i = 0; i < size; i++)
+    {
+        numberOne = Convert.ToInt32(numberSting[i].ToString());
+        mult = mult * numberOne;
+        sum = sum + numberOne;
+    }
+
+    if (mult % sum == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int[] array = new int [10];
+int number = 0;
+int i = 0;
+while (i < 10)
+{
+    array[i] = new Random().Next(10, 1000);
+    number = array[i];
+    if (Interesting(number) == 1)
+    {
+        Console.Write(number + "\t");
+        i++;
+    }
+    else
+    {
+
+    }
+}
